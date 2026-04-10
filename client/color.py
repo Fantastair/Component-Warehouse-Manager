a40c93566319828f85f22a936a568b089e9721e8
Fantastair <Fantastair@qq.com>
2026-04-10 09:49:50 +0800

    新增前端颜色管理模块

---
+++ client/color.py
@@ -0,0 +1,18 @@
+""" 颜色配置 """
+import fantas
+
+THEME = fantas.Color("#845ec2")
+SPOT_PALETTE = [
+    THEME,
+    fantas.Color("#b39cd0"),
+    fantas.Color("#fbeaff"),
+    fantas.Color("#00c9a7"),
+]
+
+LINEAR_GRADIENT_COLORS = [
+    SPOT_PALETTE[0],
+    SPOT_PALETTE[-1],
+]
+
+WHITE = fantas.Color("#ffffff")
+GRAY = fantas.Color("#888888")
